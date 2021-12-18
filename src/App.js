8ed95aabf5d8373197ae14c10a2f2bb22a1e0eca
Noemi Scotellaro <noemi.scotellaro@gmail.com>
2021-12-18 02:23:38 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import { useState, useRef } from "react"
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([])
+  const toDoListForm = useRef(null)
+  
+  const handleClickEvent = () => {
+    const newArray = [...items]
+    const form = toDoListForm.current
+    newArray.push(form["listItem"].value)
+        setItems(newArray)
+ }
+
+  const removeHandler = (index) => {
+      const newArray = [...items]
+      newArray.splice(index, 1)
+      setItems(newArray)
+  }
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="container">
+          <h1 class="title">TO DO LIST</h1>
+
+          <div>
+            <form ref={toDoListForm}>
+            <input class="input" name={'listItem'}/>
+          
+            </form>
+            <button className="button" onClick={handleClickEvent}>SUBMIT</button>
+          </div>
+
+          <ul>
+          {items.map((item, index) => {
+                return  (
+                    <>
+                        <li key={index}>
+                          {item}
+                          <button className="button remove" onClick={() => removeHandler(index)}>remove</button>
+                        </li>
+                    </>
+                )
+            })}
+          </ul>
+      </div>
+  )
 }
 
 export default App;
